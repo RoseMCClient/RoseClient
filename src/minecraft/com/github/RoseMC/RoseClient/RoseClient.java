138d369460ee43ad369c503d940a5aae8aaef536
Oliver Zhang <63158613+Zhang-Dev-byte@users.noreply.github.com>
2021-01-14 11:08:24 -0800

    Added basic login system
    Added a login system along with the MCP client.

---
+++ src/minecraft/com/github/RoseMC/RoseClient/RoseClient.java
@@ -0,0 +1,63 @@
+package com.github.RoseMC.RoseClient;
+
+import org.lwjgl.opengl.*;
+
+import com.mojang.authlib.*;
+import com.mojang.authlib.yggdrasil.*;
+
+import net.minecraft.client.*;
+import net.minecraft.util.*;
+
+import java.io.*;
+import java.net.*;
+import java.util.*;
+
+import javax.swing.*;
+
+public class RoseClient
+{
+	public static String CLIENT = "RoseClient";
+	public static String VERSION = "Alpha v1.0";
+	
+	public static void StartClient()
+	{
+		Display.setTitle(CLIENT+" "+VERSION);
+	}
+	
+	public static void OnAuth() throws Exception
+	{
+		File loginCache = new File(".login_cache");
+		
+		String username = "";
+		String password = "";
+		
+		if(loginCache.exists())
+		{
+			Scanner fileScanner = new Scanner(loginCache);
+			
+			username = fileScanner.nextLine();
+			password = fileScanner.nextLine();
+			
+			fileScanner.close();
+		}
+		else
+		{
+			username = JOptionPane.showInputDialog("Please enter your mojang account email: ");
+			password = JOptionPane.showInputDialog("Please enter your mojang account password: ");
+		}
+		
+		YggdrasilUserAuthentication auth = (YggdrasilUserAuthentication) new YggdrasilAuthenticationService(Proxy.NO_PROXY, "").createUserAuthentication(Agent.MINECRAFT);
+		
+		auth.setUsername(username);
+		auth.setPassword(password);
+		
+		auth.logIn();
+		
+		Minecraft.getMinecraft().session = new Session(auth.getSelectedProfile().getName(), auth.getSelectedProfile().getId().toString(), auth.getAuthenticatedToken(), "mojang");
+		
+		FileWriter fw = new FileWriter(loginCache);
+		fw.write(username+System.lineSeparator());
+		fw.write(password);
+		fw.close();
+	}
+}
